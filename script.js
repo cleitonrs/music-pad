b80e469890a473a387f2caafded0f7cb6b14a82d
cleitonrs <rscleitonrs@gmail.com>
2022-02-18 15:03:17 -0300

    click animation fix

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ function playSound(e) {
   key.classList.add('playing')
 }
 
-function playSound2(e) {
-  const audio = document.querySelector(`audio[data-key="${e.target.value}"]`)
-  const key = document.querySelector(`.key[data-key="${e.target.value}"]`)
-  if (!audio) return
-  audio.currentTime = 0
-  audio.play()
-  key.classList.add('playing')
-}
-
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return
   this.classList.remove('playing')
@@ -25,3 +16,11 @@ const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 window.addEventListener('keydown', playSound)
 
+let audio = document.getElementById('comma')
+
+
+
+function playSound2 () {
+  audio.currentTime = 0
+  audio.play()
+}
