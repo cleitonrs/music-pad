4a53a9448d17b9d9c991934187541acd8d076ca5
cleitonrs <rscleitonrs@gmail.com>
2022-02-18 13:13:26 -0300

    new style

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-window.addEventListener('keydown', (e) => {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`)
   const key = document.querySelector(`.key[data-key="${e.key}"]`)
   if (!audio) return
   audio.currentTime = 0
   audio.play()
   key.classList.add('playing')
-})
+}
+
+function playSound2(e) {
+  const audio = document.querySelector(`audio[data-key="${e.target.value}"]`)
+  const key = document.querySelector(`.key[data-key="${e.target.value}"]`)
+  if (!audio) return
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing')
+}
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return
@@ -14,3 +23,5 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
+
