b7cceefba5dc9961e7b8c7c4a9f4f9a38dcfd91f
cleitonrs <rscleitonrs@gmail.com>
2022-02-21 17:48:30 -0300

    updated

--- script.js
+++ script.js
@@ -16,11 +16,99 @@ const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 window.addEventListener('keydown', playSound)
 
-let audio = document.getElementById('comma')
 
+function playSound2() {
+  const audio = document.getElementById('divide')
+  audio.currentTime = 0
+  audio.play()
+}
 
+function playSound3() {
+  const audio = document.getElementById('multiply')
+  audio.currentTime = 0
+  audio.play()
+}
 
-function playSound2 () {
+function playSound4() {
+  const audio = document.getElementById('subtract')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound5() {
+  const audio = document.getElementById('seven')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound6() {
+  const audio = document.getElementById('eight')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound7() {
+  const audio = document.getElementById('nine')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound8() {
+  const audio = document.getElementById('plus')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound9() {
+  const audio = document.getElementById('four')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound10() {
+  const audio = document.getElementById('five')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound11() {
+  const audio = document.getElementById('six')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound12() {
+  const audio = document.getElementById('one')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound13() {
+  const audio = document.getElementById('two')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound14() {
+  const audio = document.getElementById('three')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound15() {
+  const audio = document.getElementById('zero')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound16() {
+  const audio = document.getElementById('comma')
+  audio.currentTime = 0
+  audio.play()
+}
+
+function playSound17() {
+  const audio = document.getElementById('enter')
   audio.currentTime = 0
   audio.play()
 }
